397b54c67c77a97ff3313cadc2e856c1918cb703
congnd173 <ndcong1703@gmail.com>
2023-09-16 02:59:12 +0700

    feat: Layout and AuthUI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,10 @@
+import Header from "@/components/Header";
+
 export default function Home() {
-  return <div className=" text-sky-500 text-3xl">Hello World!</div>;
+  return (
+    <>
+      <Header label="Home" />
+      <div className=" text-sky-500 text-3xl">Hello World!</div>
+    </>
+  );
 }
