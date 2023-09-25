a5ae4385c19654859525228179a484d8909dcdd3
THinh <thinhnbgcd191106@fpt.edu.vn>
2023-09-25 14:49:21 +0700

    feat: Load and Create Posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
+import Form from "@/components/Form";
 import Header from "@/components/Header";
+import PostFeed from "@/components/posts/PostFeed";
 
 export default function Home() {
   return (
     <>
       <Header label="Home" />
-      <div className=" text-sky-500 text-3xl">Hello World!</div>
+      <Form placeholder = "Write something" />
+      <PostFeed />
     </>
   );
 }
